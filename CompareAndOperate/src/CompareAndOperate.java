3745f33c62be1d3469b9b090c2aa84a3b03ba6da
Dushkova Kseniya <keelita201@gmail.com>
2024-09-23 11:43:43 +0300

    done

---
+++ CompareAndOperate/src/CompareAndOperate.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class CompareAndOperate {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите первое число (a): ");
+        int a = scanner.nextInt();
+
+        System.out.print("Введите второе число (b): ");
+        int b = scanner.nextInt();
+
+        // Сравнение чисел
+        if (a > b) {
+            System.out.println("a > b");
+        } else if (a < b) {
+            System.out.println("a < b");
+        } else {
+            System.out.println("a = b");
+        }
+
+        // Арифметические операции
+        System.out.println("Сложение: " + (a + b));
+        System.out.println("Вычитание: " + (a - b));
+        System.out.println("Умножение: " + (a * b));
+        if (b != 0) {
+            System.out.println("Деление: " + ((double) a / b));
+        } else {
+            System.out.println("Деление: деление на ноль невозможно");
+        }
+    }
+}
